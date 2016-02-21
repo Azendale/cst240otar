880fb7186a155bd0e43cc981bc68a8363e8bff17
Erik B. Andersen <erik.b.andersen@gmail.com>
2016-02-20 18:09:46 -0800

    Add structures for holding the runtime options of the program.

--- otar.c
+++ otar.c
@@ -13,9 +13,70 @@ void DebugOutput(int level, const char * message, ...)
     if (level <= g_debugLevel)
     {
         vfprintf(stderr, message, args);
-        //fprintf(stderr, message);
     }
     va_end(args);
+}
+
+struct t_string_list
+{
+    char ** list;
+    int size;
+}
+
+void Construct_t_string_list(struct t_string_list * newStruct);
+void Construct_t_string_list(struct t_string_list * newStruct)
+{
+   newStruct->size = 0; 
+   newStruct->list = NULL; 
+}
+
+void Destruct_t_string_list(struct t_string_list * oldStruct);
+void Destruct_t_string_list(struct t_string_list * oldStruct)
+{
+    // Up to user of struct to clean up strings they have put in, unless
+    // they specifically asked for it with FreeStrings_t_string_list
+    free(oldStruct->list);
+    oldStruct->list = NULL;
+}
+
+void FreeStrings_t_string_list(struct t_string_list * oldStruct);
+void FreeStrings_t_string_list(struct t_string_list * oldStruct)
+{
+    char * currentString = NULL;
+    for (int index = 0; index < oldStruct->size; ++index)
+    {
+        free((oldStruct->list)[index]);
+        (oldStruct->list)[index] = NULL;
+    }
+}
+
+void AddString_t_string_list(struct t_string_list * container, const char * string);
+void AddString_t_string_list(struct t_string_list * container, const char * string)
+{
+    ++(container->size);
+    size_t newSizeBytes = sizeof(char *)*(container->size)
+    (container->list) = realloc(container->list, newSizeBytes);
+    if (NULL != (container->list))
+    {
+        (container->list)[(container->size)-1] = string;
+    }
+    else
+    {
+        container->size = 0;
+        exit(127);
+    }
+}
+
+int GetStringCount_t_string_list(struct t_string_list * container);
+int GetStringCount_t_string_list(struct t_string_list * container)
+{
+    return container->size;
+}
+
+const char * GetStringByIndex_t_string_list(struct t_string_list * container, int index);
+const char * GetStringByIndex_t_string_list(struct t_string_list * container, int index)
+{
+    return (container->list)[index];
 }
 
 struct t_program_opts
@@ -28,6 +89,7 @@ struct t_program_opts
     bool showContentsShort;
     bool extractFiles;
     bool deleteFiles;
+    struct t_string_list files;
 };
 
 void Construct_t_program_opts(struct t_program_opts *newStruct);
@@ -41,6 +103,25 @@ void Construct_t_program_opts(struct t_program_opts *newStruct)
     newStruct->showContentsShort = false;
     newStruct->extractFiles = false;
     newStruct->deleteFiles = false; 
+    Construct_t_string_list(&(newStruct->files));
+}
+
+void AddFile_t_program_opts(struct t_program_opts options, const char * string);
+void AddFile_t_program_opts(struct t_program_opts options, const char * string)
+{
+    AddString_t_string_list(&(newStruct->files), string);
+}
+
+int GetFileCount_t_program_opts(struct t_program_opts options);
+int GetFileCount_t_program_opts(struct t_program_opts options)
+{
+    return GetStringCount_t_string_list(&(options->files));
+}
+
+const char * GetFileNameByIndex_t_program_opts(struct t_program_opts options, int index);
+const char * GetFileNameByIndex_t_program_opts(struct t_program_opts options, int index)
+{
+    return GetStringByIndex_t_string_list(&(options->files), index);
 }
 
 void parseopt(int argc, char ** argv, struct t_program_opts *options);
@@ -81,6 +162,12 @@ void parseopt(int argc, char ** argv, struct t_program_opts *options)
         {
            options->deleteFiles = true; 
         }
+    }
+    if (optind < argc) {
+    DebugOutput(2, "non getopt parameters:\n");
+    for (/*optind already set*/; optind < argc; ++optind)
+        printf ("%s \n", argv[optind]);
+        AddFile_t_program_opts(options, argv[optind]);
     }
 }
 
